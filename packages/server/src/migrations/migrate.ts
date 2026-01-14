5edbae5fec2c51809fe74d594b4414d8166cb645
Afreen Sikandara <afreen.sikandara@data4life-asia.care>
2026-01-14 15:37:19 +0800

    Revert

--- packages/server/src/migrations/migrate.ts
+++ packages/server/src/migrations/migrate.ts
@@ -9,8 +9,8 @@ import {
   isResourceTypeSchema,
   SearchParameterType,
 } from '@medplum/core';
-import { SEARCH_PARAMETER_BUNDLE_FILES } from '@medplum/definitions';
-import { readJson } from '../definitions/src/index';
+import { readJson, SEARCH_PARAMETER_BUNDLE_FILES } from '@medplum/definitions';
+// import { readJson } from '../definitions/src/index';
 import { Bundle, ResourceType, SearchParameter } from '@medplum/fhirtypes';
 import { readdirSync, writeFileSync } from 'fs';
 import { resolve, dirname } from 'path';
